c46c8aba1bfdcf53ca40aafb58faa72d4a7b2d88
Dell <Dell@DESKTOP-C0PBQMC>
2022-07-19 11:43:53 +0530

    first commit

---
+++ org.TYSS.Project/src/test/java/org/TYSS/Project/example/ExampleTest1.java
@@ -0,0 +1,18 @@
+package org.TYSS.Project.example;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest1 {
+	@Test
+public void welcometest() {
+	System.out.println("welcome to testng");
+}
+	@Test
+public void practiceTest() {
+	System.out.println("practice method");
+}
+	@Test
+public void alpha() {
+	System.out.println("alpha executed");
+}
+}
---
+++ org.TYSS.Project/src/test/java/org/TYSS/Project/example/ExampleTest2.java
@@ -0,0 +1,22 @@
+package org.TYSS.Project.example;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest2 {
+	@Test
+public void PracticeTest() {
+	System.out.println("practice test ");
+}
+	@Test
+public void PracticeTest1() {
+	System.out.println("practice test 1");
+}
+	@Test
+public void PracticeTest2() {
+	System.out.println("practice test 2");
+}
+	@Test
+public void PracticeTest3() {
+	System.out.println("practice test 3");
+}
+}
